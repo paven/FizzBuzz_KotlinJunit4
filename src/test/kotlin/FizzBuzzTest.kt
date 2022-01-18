14c55ccfde706d93be91356b6ac9c0e4952e7849
Patrik Gustafsson <PGUSTA49@MGC12Z29XMLVCG.dunton.ford.com>
2022-01-18 09:10:28 +0100

    Idea FizzBuzz Kotlin junit4 Project

---
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -0,0 +1,14 @@
+import org.junit.Test
+
+internal class FizzBuzzTest {
+
+    @Test
+    fun exampleTrue(){
+        assert(true)
+    }
+
+    @Test
+    fun exampleFalse(){
+        assert(false)
+    }
+}
