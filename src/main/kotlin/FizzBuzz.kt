dbbf8ea24d5c0fce493b6d036e19937a16de0e79
Patrik Gustafsson <PGUSTA49@MGC12Z29XMLVCG.dunton.ford.com>
2022-01-19 13:05:46 +0100

    Kata round 1, Code with me

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,2 +1,7 @@
 class FizzBuzz {
+
+    fun returnNumber(number: Int):String {
+        return if(number% 3 == 0)"Fizz"
+        else "1"
+    }
 }
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,14 +1,34 @@
 import org.junit.Test
+import kotlin.test.assertEquals
 
+
+/*
+  - Peter
+  - Alagu
+ Typist - Scott
+ Navigator - Michael
+ Uryel
+ Srinivas
+ Patrik
+
+ Print 1 as 1
+ Print 3 as Fizz
+ Print 4 as 4
+ Print 5 as Buzz
+
+Overiview
+red
+Green
+Refactor
+ */
 internal class FizzBuzzTest {
-
     @Test
-    fun exampleTrue(){
-        assert(true)
+    fun printOneAsOne(){
+        assertEquals("1", FizzBuzz().returnNumber(1))
     }
 
     @Test
-    fun exampleFalse(){
-        assert(false)
+    fun print3AsFizz(){
+        assertEquals("Fizz", FizzBuzz().returnNumber(3))
     }
 }
