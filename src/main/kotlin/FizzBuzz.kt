967c669283ea6fc7b3fd0909231750567eabe41b
Patrik Gustafsson <patrik.gustafsson@proagile.se>
2022-01-25 08:36:38 +0100

    verify metod returns true!

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,2 +1,14 @@
+/*
+Overview
+- Look at problem statment
+- List Test cases
+- Update the order for navigator typist
+
+Red - failing test
+Green - all test
+
+Refactor - Make it do the same thing but somewhat better.
+ */
+
 class FizzBuzz {
 }
