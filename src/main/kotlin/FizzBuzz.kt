d59d6f570b2f2d1d592e2fd0355cfeb076dfaff2
Patrik Gustafsson <patrik.gustafsson@proagile.se>
2022-01-25 14:23:12 +0100

    After 2022-01-25

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -4,11 +4,29 @@ Overview
 - List Test cases
 - Update the order for navigator typist
 
+    - Print numbers from 1 to 100
+    - Multiples of 3 should be printed as "Fizz"
+    - Multiples of 5 should be printed as "Buzz"
+    - Multiples of both 3 & 5 should be printed as "FizzBuzz"
+
+
 Red - failing test
 Green - all test
 
 Refactor - Make it do the same thing but somewhat better.
+
+Driver - Daniel
+Navigator - Jegan
+Next -  Arun
+
+
+
+Kata - https://www.sammancoaching.org/kata_descriptions/fizzbuzz.html
  */
 
 class FizzBuzz {
+
+    fun printNumbers(numberOfLines: Int): Boolean {
+        return false
+    }
 }
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -1,14 +1,18 @@
+import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertTrue
 
 internal class FizzBuzzTest {
 
-    @Test
-    fun exampleTrue(){
-        assert(true)
+    private lateinit var subject: FizzBuzz
+
+    @Before
+    fun setUp() {
+        subject = FizzBuzz()
     }
 
     @Test
-    fun exampleFalse(){
-        assert(false)
+    fun printNumbersFrom1to100() {
+        assertTrue(subject.printNumbers(100))
     }
 }
