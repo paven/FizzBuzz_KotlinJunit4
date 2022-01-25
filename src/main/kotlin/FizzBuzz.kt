17bfa712bc85e2ac347dca8d9b6127ffd09b54a0
Patrik Gustafsson <patrik.gustafsson@proagile.se>
2022-01-25 16:12:57 +0100

    After Kata 2022-01-25

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,7 +1,10 @@
 class FizzBuzz {
 
-    fun returnNumber(number: Int):String {
-        return if(number% 3 == 0)"Fizz"
-        else "1"
-    }
+    fun returnNumber(number: Int): String =
+        when {
+            (number % 15 == 0) -> "FizzBuzz"
+            (number % 5 == 0) -> "Buzz"
+            (number % 3 == 0) -> "Fizz"
+            else -> number.toString()
+        }
 }
--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -3,20 +3,27 @@ import kotlin.test.assertEquals
 
 
 /*
-  - Peter
-  - Alagu
- Typist - Scott
- Navigator - Michael
- Uryel
- Srinivas
- Patrik
+  Peter
+  Alex
+  Next - Scott
+  Alagu
+  Uryel
+  Srinivas
+  Patrik
 
- Print 1 as 1
- Print 3 as Fizz
- Print 4 as 4
- Print 5 as Buzz
+ * Print 1 as 1
+ * Print 3 as Fizz
+ * Print 4 as 4
+ * Print 5 as Buzz
+ * Print 6 as Fizz
+ * Print 15 as FizzBuzz
+ * Print 20 as Buzz
+ * Print 45 as FizzBuzz
 
-Overiview
+ Print function returns 100 lines
+ The first line is correct
+
+Overview
 red
 Green
 Refactor
@@ -28,7 +35,32 @@ internal class FizzBuzzTest {
     }
 
     @Test
+    fun print4As4(){
+        assertEquals("4", FizzBuzz().returnNumber(4))
+    }
+
+    @Test
     fun print3AsFizz(){
         assertEquals("Fizz", FizzBuzz().returnNumber(3))
+    }
+
+    @Test
+    fun print5AsBuzz(){
+        assertEquals("Buzz", FizzBuzz().returnNumber(5))
+    }
+
+    @Test
+    fun print15AsFizzBuzz(){
+        assertEquals("FizzBuzz", FizzBuzz().returnNumber(15))
+    }
+
+    @Test
+    fun print20AsBuzz(){
+        assertEquals("Buzz", FizzBuzz().returnNumber(20))
+    }
+
+    @Test
+    fun print45AsFizzBuzz(){
+        assertEquals("FizzBuzz", FizzBuzz().returnNumber(45))
     }
 }
